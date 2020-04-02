31ebec4191c24552a7358cc225d1c5c83be39033
zetay.lim <zetay.lim@alibaba-inc.com>
2020-04-02 20:11:47 +0800

    fix: 注入bwip-fonts

--- webpack/config/other.js
+++ webpack/config/other.js
@@ -6,9 +6,15 @@ const get = require('lodash/get');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const systemModulePlugin = require('@saasfe/saas-webpack-system-module');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { ROOT_PATH, SAAS_CONFIG, ASSETS_URL } = require('../util/const');
+const { ROOT_PATH, SAAS_CONFIG, ASSETS_URL, CDN_BASE } = require('../util/const');
 
 const microAppName = get(SAAS_CONFIG, 'microAppName', '');
+
+const defineProcess = {
+  env: {
+    PUBLIC_URL: `${CDN_BASE}/web/dist`,
+  },
+};
 
 module.exports = function(config){
   config.plugins = config.plugins || [];
@@ -18,6 +24,10 @@ module.exports = function(config){
   config.plugins.push(new webpack.DefinePlugin({
     'MICRO_APPNAME': JSON.stringify(microAppName),
     'PUBLIC_PATH': JSON.stringify(ASSETS_URL),
+    // 用于bwip-fonts
+    process: JSON.stringify(defineProcess),
+    'process.env': JSON.stringify(defineProcess.env),
+    'process.env.PUBLIC_URL': JSON.stringify(defineProcess.env.PUBLIC_URL),
   }));
   config.plugins.push(new MiniCssExtractPlugin());
 };
