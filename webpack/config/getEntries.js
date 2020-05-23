b56496360c2947fae61ac944afa06c2a4e49bd04
zetay.lim <zetay.lim@alibaba-inc.com>
2020-05-23 17:08:19 +0800

    fix: 变量名处理

--- webpack/config/getEntries.js
+++ webpack/config/getEntries.js
@@ -56,7 +56,7 @@ module.exports = function (startupParam) {
         [chunkName]: resolveEntry.concat(entryValue),
       },
       output: {
-        library: `__weapp__${microAppName.replace(/-/g, '_')}__${chunkName}`,
+        library: `__weapp__${microAppName.replace(/-/g, '_')}__${chunkName.replace(/-/g, '_')}`,
       },
     };
 
